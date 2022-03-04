a93bea96c18b1c0a16e4c8130ed3b6020d942eec
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-04 20:15:41 +0100

    primeras clases

---
+++ EjercicioHerencia/Becario.vb
@@ -0,0 +1,21 @@
+﻿Option Explicit On
+Option Strict On
+
+Public Class Becario : Inherits Empleado
+
+    ''' <summary>
+    ''' Calcula salario mes del becario
+    ''' </summary>
+    Public Overrides Sub CalculaSalarioMes()
+        pvSalarioMes = SalarioHora * HorasTrabajadas.Hours
+    End Sub
+    ''' <summary>
+    ''' COnstructor de la clase hija
+    ''' </summary>
+    ''' <param name="nombre"></param>
+    ''' <param name="dni"></param>
+    ''' <param name="salarioHora"></param>
+    Sub New(nombre As String, dni As String, salarioHora As Double, horasTrabajadas As TimeSpan)
+        MyBase.New(nombre, dni, salarioHora, horasTrabajadas)
+    End Sub
+End Class
---
+++ EjercicioHerencia/Empleado.vb
@@ -0,0 +1,34 @@
+﻿Option Explicit On
+Option Strict On
+Public MustInherit Class Empleado
+
+    Protected pvSalarioMes As Double
+    Public Property Nombre As String
+    Public Property HorasTrabajadas As TimeSpan
+    Public Property Dni As String
+    Public Property SalarioHora As Double
+    Public ReadOnly Property SalarioMes As Double 'objeto.SalarioMes = value
+        Get
+            Return pvSalarioMes
+        End Get
+        'Set(value As Double)
+        '    pvSalarioMes = value
+        'End Set
+    End Property
+
+    Public MustOverride Sub CalculaSalarioMes()
+
+    ''' <summary>
+    ''' Constructor del padre Empleado
+    ''' </summary>
+    ''' <param name="nombre"></param>
+    ''' <param name="dni"></param>
+    ''' <param name="salarioHora"></param>
+    Sub New(nombre As String, dni As String, salarioHora As Double, horastrabajadas As TimeSpan)
+        Me.Nombre = nombre
+        Me.Dni = dni
+        Me.SalarioHora = salarioHora
+        Me.HorasTrabajadas = horastrabajadas
+    End Sub
+
+End Class
