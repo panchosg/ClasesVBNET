e8cd81667e916f3355b378846122b753ce6b64a3
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-07 20:05:50 +0100

    Tema 4_8

---
+++ Tema4_8/IImpresora.vb
@@ -0,0 +1,21 @@
+﻿Public Interface IImpresora
+
+    Property EstaConectada As Boolean
+    Property ErrorImpresora As Boolean
+    Property EncederLedsRojos As Boolean
+    Property EncederLedsVerdes As Boolean
+
+    ''' <summary>
+    ''' Ejecuta comando de impresión
+    ''' </summary>
+    ''' <param name="textoImprimir">texto que se va a imprimir</param>
+    ''' <returns>True si fue correcta la impresión</returns>
+    Function Imprimir(textoImprimir As String) As Boolean
+    ''' <summary>
+    ''' Cancelar impresión
+    ''' </summary>
+    ''' <returns>True si se pudo cancelar la impresión</returns>
+    Function Cancelar() As Boolean
+
+
+End Interface
---
+++ Tema4_8/Program.vb
@@ -0,0 +1,11 @@
+Option Explicit On
+Option Strict On
+
+Module Program
+    Sub Main(args As String())
+
+        'Imprimir un hola mundo en diferentes tipos de dispositivo de impresión
+
+
+    End Sub
+End Module
