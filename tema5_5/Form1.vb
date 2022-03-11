08157b583d8dd751faabfc178770bc23ae8ec477
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-11 19:54:56 +0100

    tema 5_5 button

--- tema5_5/Form1.vb
+++ tema5_5/Form1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Boton activado", "Titulo Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
 End Class
