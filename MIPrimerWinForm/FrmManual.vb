fb91ef04ff030ed33046c37ae3476a68acaadabb
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-11 17:19:27 +0100

    tema 5_3

---
+++ MIPrimerWinForm/FrmManual.vb
@@ -0,0 +1,43 @@
+﻿Option Explicit On
+Option Strict On
+Public Class FrmManual : Inherits Form
+
+    Friend WithEvents Button1 As Button
+
+    Public Sub New()
+        Text = "Ventana Manual"
+        Name = "frmManual"
+        ClientSize = New Size(300, 50)
+        StartPosition = FormStartPosition.CenterScreen
+    End Sub
+
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(347, 142)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FrmManual
+        '
+        Me.ClientSize = New System.Drawing.Size(689, 353)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "FrmManual"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub FrmManual_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
---
+++ MIPrimerWinForm/Program.vb
@@ -0,0 +1,13 @@
+﻿Option Explicit On
+Option Strict On
+Module Program
+    Public Sub Main()
+        Dim frmManual As FrmManual
+
+        frmManual = New FrmManual()
+        frmManual.Text = "Probando este código"
+        'frmManual.Show() No deja permanente la ejecución del programa
+        Application.Run(frmManual)
+    End Sub
+
+End Module
