34015836fec3b2dae53f7a98a9c6dfccf461d73a
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-11 19:03:25 +0100

    Tema 5_4

---
+++ Tema5_4/FormularioManual.vb
@@ -0,0 +1,12 @@
+﻿Public Class FormularioManual : Inherits Form
+    Sub New()
+
+    End Sub
+
+    Public Sub PropiedadesIniciales()
+        Text = "Ventana Manual"
+        ClientSize = New Size(800, 600)
+        Name = "ventanaManual"
+    End Sub
+
+End Class
---
+++ Tema5_4/Program.vb
@@ -0,0 +1,11 @@
+﻿Module Program
+    Sub Main()
+        Dim formularioManual As FormularioManual
+
+        formularioManual = New FormularioManual()
+        formularioManual.PropiedadesIniciales()
+        Application.Run(formularioManual)
+
+    End Sub
+
+End Module
