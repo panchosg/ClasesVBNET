67e558e117884fbd977e2a03522bd1f3b844e037
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-16 20:23:52 +0100

    Tema5_9. Practica Formularios MDI

---
+++ Tema5_9/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class FormPrincipal
+    Private Sub FormPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub AdicionarJugadoresToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AdicionarJugadoresToolStripMenuItem.Click
+        Dim formAdicionarJ As FrmAdicionarJ
+        formAdicionarJ = New FrmAdicionarJ()
+        formAdicionarJ.Show() 'Indica que se muestre el formulario
+    End Sub
+End Class
---
+++ Tema5_9/FrmAdicionarJ.vb
@@ -0,0 +1,3 @@
+﻿Public Class FrmAdicionarJ
+
+End Class
