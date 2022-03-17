ab4bf93612a854a8f04189ee6f0a4d848102b2ce
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-17 20:13:26 +0100

    Adicion clase Jugador

---
+++ Tema5_9/Modelos/Jugador.vb
@@ -0,0 +1,8 @@
+﻿Public Class Jugador
+
+    Public Property Nombre As String
+    Public Property Dorsal As Integer
+    Public Property LicenciaEnVigor As Boolean
+    Public Property Sueldo As Double
+
+End Class
