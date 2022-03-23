b0e490cec7db261359d0184fd048d583b2afe2da
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-23 21:35:44 +0100

    tema6_3

---
+++ Tema6_3/Consultas SQL.sql
@@ -0,0 +1,14 @@
+﻿USE world;
+-- SELECT * FROM country WHERE Name = "Venezuela";
+-- SELECT * FROM country ORDER BY LifeExpectancy;
+-- SELECT * FROM country WHERE code2 LIKE "Ua%";
+/* SELECT * FROM country WHERE IndepYear >= 1950 AND IndepYear <=1960  order by Name;
+SELECT * FROM country WHERE IndepYear between 1950 and 1960 order by Name; */
+-- SELECT * FROM country WHERE name = "Benin" OR name = "Cambodia";
+-- select * from country where governmentForm = "Republic" And LifeExpectancy >50.0 order by LifeExpectancy;
+-- SELECT * FROM country where Continent in ("Africa","Asia","Oceania");
+-- SELECT * FROM country where IndepYear in (1950,1960,1970) order by Continent;
+-- SELECT * FROM country WHERE NOT GovernmentForm = "Republic" AND NOT GovernmentForm = "Federal Republic";
+-- SELECT * FROM country WHERE GovernmentForm like "%Socialist%";
+
+
