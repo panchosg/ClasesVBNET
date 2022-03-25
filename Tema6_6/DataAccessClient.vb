61282523f2036e57639d50fe2d671953976e2e34
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-25 21:34:21 +0100

    tema 6_6

---
+++ Tema6_6/DataAccessClient.vb
@@ -0,0 +1,27 @@
+﻿
+Imports System.Data.SqlClient
+
+Public Class DataAccessClient
+    Private ReadOnly _connectionString As String
+
+    Sub New(server As String, database As String, user As String, passwd As String)
+        _connectionString = $"server={server};database={database};uid={user};pwd={passwd}"
+    End Sub
+
+    Public Function ConnectionTest() As Boolean
+        Dim oConnection As SqlConnection
+        Try
+            oConnection = New SqlConnection()
+            oConnection.ConnectionString = _connectionString
+            oConnection.Open()
+            Debug.WriteLine("Conexión establecida")
+            oConnection.Close()
+            Debug.WriteLine("Conexión cerrada")
+        Catch ex As Exception
+            Debug.WriteLine("Hubo un error en la conexión")
+        End Try
+
+
+    End Function
+
+End Class
