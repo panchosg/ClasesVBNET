e239dcc53c79d04a6e29e388c0507f2ebd2c47fd
Fernando Javier Vera Pèrez <Panchosg2@hotmail.com>
2022-03-25 20:01:28 +0100

    Tema 6_3

---
+++ Tema6_3/ConsultasMSSQL.sql
@@ -0,0 +1,14 @@
+﻿/****** Script para el comando SelectTopNRows de SSMS  ******/
+USE prueba;
+-- INSERT INTO gestores_bd (nombre,lanzamiento,desarrollador) values('Otra BBDDD',1900,'Propio'
+-- SELECT TOP(1000) * FROM gestores_bd;
+-- DELETE FROM gestores_bd WHERE id=7;
+-- DELETE FROM gestores_bd; JAMAS LO HAGAN SIN EL WHERE!!!!
+
+/*INSERT INTO gestores_bd (nombre,lanzamiento,desarrollador) values ('MSSQL',1989,'papá Microsoft');
+INSERT INTO gestores_bd (nombre,lanzamiento,desarrollador) values ('MySql',1992,'Linux Torval');
+INSERT INTO gestores_bd (nombre,lanzamiento,desarrollador) values ('DB2',1970,'IBM');
+INSERT INTO gestores_bd (nombre,lanzamiento,desarrollador) values ('MariaDB',2005,'Creador MySql');
+INSERT INTO gestores_bd (nombre,lanzamiento,desarrollador) values ('MongoDB',2000,'AnyWhere');
+INSERT INTO gestores_bd (nombre,lanzamiento,desarrollador) values ('Sphinx',2000,'Un Ruso');*/
+-- UPDATE gestores_bd SET lanzamiento = 1900; 
